cbc0713430ace399174370219e2f64fc64460bd6
Wojciech Nagórski <wojtpl2@gmail.com>
2018-11-09 11:25:50 +0100

    Add test

---
+++ samples/BenchmarkDotNet.Samples/IntroTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNet.Samples
+{
+    [CoreJob]
+    [ClrJob]
+    [MonoJob("mono", @"C:\Program Files\Mono\bin\mono.exe")]
+    public class IntroTest
+    {
+        [Benchmark]
+        public void ERange()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                Enumerable.Range(0, N);
+            }
+        }
+
+        public int N { get; set; } = 1000; 
+    }
+}
